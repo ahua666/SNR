d1f7da9c424f4b71600191e5fd4f220e5706c014
ahua666 <2524329629@qq.com>
2022-11-05 23:36:46 +0800

    备份

---
+++ src/main/java/com/zgy/snr/common/IDataComplete.java
@@ -0,0 +1,14 @@
+package com.zgy.snr.common;
+
+/**
+ * 是否完成数据接收
+ *
+ * @author zgy
+ * @data 2021/3/18 11:59
+ */
+
+@FunctionalInterface
+public interface IDataComplete {
+
+    Boolean isComplete(String recv);
+}
---
+++ src/main/java/com/zgy/snr/common/IReplyCallback.java
@@ -0,0 +1,14 @@
+package com.zgy.snr.common;
+
+/**
+ * 消息校验接口
+ *
+ * @author zgy
+ * @data 2021/3/16 10:38
+ */
+
+@FunctionalInterface
+public interface IReplyCallback {
+
+    Boolean recvData(String recv);
+}
